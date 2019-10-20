716b81c83c6940c0cb793ff78eb7bd26477930d6
Jihed <Jihed@DESKTOP-BPFGIQA>
2019-10-20 02:09:16 +0100

    Add Employe avec  classe  etat civil

--- Piadvy-ejb/src/main/java/tn/advyteam/entities/Employee.java
+++ Piadvy-ejb/src/main/java/tn/advyteam/entities/Employee.java
@@ -1,18 +1,39 @@
 package tn.advyteam.entities;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
-public class Employee {
+public class Employee implements Serializable{
 
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private int id;
-	private String nom;
 	
-	public Employee() {
-		// TODO Auto-generated constructor stub
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String email;
+	private Boolean isActif;
+	private String password;
+	@Temporal(TemporalType.DATE)
+	private Date datenaissance;
+	private Etatcivil etatcivil ;
+	
+	
+	
+	public Employee(int id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
 	}
 
 	public int getId() {
@@ -30,6 +51,76 @@ public class Employee {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getIsActif() {
+		return isActif;
+	}
+
+	public void setIsActif(Boolean isActif) {
+		this.isActif = isActif;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDatenaissance() {
+		return datenaissance;
+	}
+
+	public void setDatenaissance(Date datenaissance) {
+		this.datenaissance = datenaissance;
+	}
+
+	
+	public Employee(int id, String nom, String prenom, String adresse, String email, Boolean isActif, String password,
+			Date datenaissance,Etatcivil etatcivil) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.email = email;
+		this.isActif = isActif;
+		this.password = password;
+		this.datenaissance = datenaissance;
+		this.etatcivil = etatcivil;
+	}
+
+	public Employee() {
+		super();
+	}
+	
 	
 	
 	
