5bdaad369924a3594e02cabc46708c6a6f6930af
NadaElAdel <nada.eladel@esprit.tn>
2019-10-21 17:55:32 +0200

    first nada commit

---
+++ Piadvy-ejb/src/main/java/tn/advyteam/entities/Formation.java
@@ -0,0 +1,15 @@
+package tn.advyteam.entities;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Formation implements Serializable {
+   
+}
