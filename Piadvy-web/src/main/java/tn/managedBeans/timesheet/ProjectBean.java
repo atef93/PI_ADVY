e1e11d13d5aed86f4cb2c99ca6f03a3b6af35092
Atef Jlassi <ateef.jlassi@gmail.com>
2019-10-26 14:05:43 +0100

    fix problems of timesheet views 1

---
+++ Piadvy-web/src/main/java/tn/managedBeans/timesheet/ProjectBean.java
@@ -0,0 +1,125 @@
+package tn.managedBeans.timesheet;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import tn.advyteam.entities.Manager;
+import tn.advyteam.entities.Projet;
+import tn.advyteam.service.GestionTimesheetRemote;
+
+@ManagedBean
+public class ProjectBean implements Serializable{
+	
+	
+	@EJB
+	GestionTimesheetRemote timesheetServiceImp;
+	
+	private String titre;
+	private String description;
+	private List<Projet> projets=new ArrayList<Projet>();
+	private int idManager=1;
+	private int idProjet;
+	private Projet selectedProjet;
+	
+	@PostConstruct
+	public void init() {
+		projets = timesheetServiceImp.getAllProjectsByManager(idManager);
+	}
+	
+	
+	public ProjectBean() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getTitre() {
+		return titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public List<Projet> getProjets() {
+		return projets;
+	}
+	public void setProjets(List<Projet> projets) {
+		this.projets = projets;
+	}
+	
+	
+	public int getIdManager() {
+		return idManager;
+	}
+
+
+	
+	
+	
+	public int getIdProjet() {
+		return idProjet;
+	}
+
+
+	public void setIdProjet(int idProjet) {
+		this.idProjet = idProjet;
+	}
+
+
+	public void setIdManager(int idManager) {
+		this.idManager = idManager;
+	}
+
+
+	
+	
+
+
+	public Projet getSelectedProjet() {
+		return selectedProjet;
+	}
+
+
+	public void setSelectedProjet(Projet selectedProjet) {
+		this.selectedProjet = selectedProjet;
+	}
+
+
+	public void addProject() throws IOException {
+		Projet projet = new Projet();
+		projet.setTitre(titre);
+		projet.setDescription(description);
+		Manager manager = new Manager();
+		manager.setId(idManager);
+		projet.setCreatedBy(manager);
+		timesheetServiceImp.addProject(projet);
+		projets.add(projet);
+		FacesContext.getCurrentInstance().getExternalContext().redirect("listeProjet.xhtml");
+
+	}
+	
+	public String voirProjet(Projet projet) {
+		selectedProjet=projet;
+		return "voirProjet.xhtml?faces-redirect=true";
+	}
+	
+	
+	
+	public void supprimerProjet(int id) throws IOException {
+		timesheetServiceImp.deleteProjectById(id);
+		FacesContext.getCurrentInstance().getExternalContext().redirect("listeProjet.xhtml");
+
+	}
+	
+}
--- Piadvy-web/src/main/java/tn/managedBeans/timesheet/TimesheetBean.java
+++ Piadvy-web/src/main/java/tn/managedBeans/timesheet/TimesheetBean.java
@@ -2,17 +2,76 @@ package tn.managedBeans.timesheet;
 
 import java.io.IOException;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FlowEvent;
+
+import tn.advyteam.entities.Developpeur;
+import tn.advyteam.entities.Timesheet;
 
 @ManagedBean
 @ViewScoped
 public class TimesheetBean {
 
+	private Developpeur developpeur;
+	private Timesheet timesheet;
+	private boolean skip;
+	
 	
 	public void index() throws IOException {
 		
 		FacesContext.getCurrentInstance().getExternalContext().redirect("footer.xhtml");
 	}
+
+
+	
+    public void save() {        
+        FacesMessage msg = new FacesMessage("Successful", "Welcome :" + developpeur.getNom());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+	
+	public Developpeur getDeveloppeur() {
+		return developpeur;
+	}
+
+
+	public Timesheet getTimesheet() {
+		return timesheet;
+	}
+
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+
+	public void setDeveloppeur(Developpeur developpeur) {
+		this.developpeur = developpeur;
+	}
+
+
+	public void setTimesheet(Timesheet timesheet) {
+		this.timesheet = timesheet;
+	}
+
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+	
+    public String onFlowProcess(FlowEvent event) {
+        if(skip) {
+            skip = false;   //reset in case user goes back
+            return "confirm";
+        }
+        else {
+            return event.getNewStep();
+        }
+    }
+	
+	
+	
 }
